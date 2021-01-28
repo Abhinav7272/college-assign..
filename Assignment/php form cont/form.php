330df05ddbd0d4e2b7569e65041cafad2dfd6094
abhinav <gaurabhinav54@gmail.com>
2021-01-28 11:31:48 +0530

    style.css add

--- Assignment/php form cont/form.php
+++ Assignment/php form cont/form.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 $name = ''; 
 $email = ''; 
@@ -21,7 +18,15 @@ $interest = '';
 		$email = ''; 
 	}
 	if (isset($_POST['Contact'])) {
-        $contact = $_POST['Contact'];
+		
+		$contact = $_POST['Contact'];
+		if(strlen($contact)<10){
+			echo "<script>alert('contact number should contains 10 digit')</script>" ;
+
+		}
+		if(! (is_numeric($contact))){
+			echo "<script>alert('contact number should contains numeric values')</script>" ;
+		}
 	}
 	else{
 		$contact = ''; 
@@ -42,19 +47,44 @@ $interest = '';
 	if (isset($_POST['Interest'])) {
 		$interest = $_POST['Interest'];
 		$interest = implode(" , ", $interest); 
+		if (substr_count($interest,",")<1){
+			echo "<script>alert('select atleast two intrests')</script>" ;
+		}
+	    
 	}
 	else{
 		$interest = '';  
-    }
-    if(! (is_numeric($contact))){
-        echo "<script>alert('contact number should contains numeric values')</script>" ;
-    }
-   if (substr_count($interest,",")<1){
-    echo "<script>alert('select atleast two intrests')</script>" ;
-}
-    
+    }   
 ?>
-<table border="2" cellpadding = "10" align="center">
+<link rel="stylesheet" href="style.css">
+<form method="post">	
+<table id = "frm" align = "center" border = "2" cellspacing = " 20">
+	<tr><td>Name <td><input type="text" placeholder="Enter Name" name = "Name" required></td></tr>
+	<tr><td>Email <td><input type="email" placeholder="Enter Email" name = "Email" required></td></tr>
+	<tr><td>Contact <td><input  type="tel"  minlength="10" maxlength="10"  placeholder="Enter Contact" name = "Contact"required></td></tr>
+	<tr><td>City 
+		<td><select name = "City"required>
+			<option>Select City</option>
+			<option value="Dehradun">Dehradun</option>
+			<option value="Delhi">Delhi</option>
+			<option value="Jaipur">Jaipur</option>
+		</select>
+	</td></tr>
+	<tr><td>Course <td><input type="text" placeholder="Enter Course Name" name = "Course" required=""></td></tr>
+	<tr><td>Interests
+	<td>
+		Programming <input type="checkbox" name="Interest[]" value = "Programming">
+		Coding <input type="checkbox" name="Interest[]" value = "Coding">
+		Reading <input type="checkbox" name="Interest[]" value = "Reading">
+		Music <input type="checkbox" name="Interest[]" value = "Music">
+		Swimming <input type="checkbox" name="Interest[]" value = "Swimming">
+		Day Dreaming <input type="checkbox" name="Interest[]" value = "Day Dreaming">
+	</td></tr>
+	<tr><td>Submit <td><input width="400px" type="Submit" name = "Sub"></td></tr>
+
+</table>
+<div>
+<table border="4" cellpadding = "10" align="center">
 	<tr>
 		<td>Name</td>
 		<td>Email</td>
@@ -72,36 +102,5 @@ $interest = '';
 		<td><?php echo $interest ?></td>
 	</tr>
 </table>
-<form method="post">	
-<table id = "frm" align = "center" border = "2" cellspacing = " 20">
-	<!-- 1 -->
-	<tr><td>Name <td><input type="text" placeholder="Enter Name" name = "Name" required></td></tr>
-	<!-- 2 -->
-	<tr><td>Email <td><input type="email" placeholder="Enter Email" name = "Email" required></td></tr>
-	<!-- 3 -->
-	<tr><td>Contact <td><input  type="tel"  minlength="10" maxlength="10"  placeholder="Enter Contact" name = "Contact"required></td></tr>
-	<!-- 4 -->
-	<tr><td>City 
-		<td><select name = "City"required>
-			<option>Select City</option>
-			<option value="Dehradun">Dehradun</option>
-			<option value="Delhi">Delhi</option>
-			<option value="Jaipur">Jaipur</option>
-		</select>
-	</td></tr>
-	<!-- 5 -->
-	<tr><td>Course <td><input type="text" placeholder="Enter Course Name" name = "Course" required=""></td></tr>
-	<!-- 6 -->
-	<tr><td>Interests
-	<td>
-		Programming <input type="checkbox" name="Interest[]" value = "Programming">
-		Coding <input type="checkbox" name="Interest[]" value = "Coding">
-		Reading <input type="checkbox" name="Interest[]" value = "Reading">
-		Music <input type="checkbox" name="Interest[]" value = "Music">
-		Swimming <input type="checkbox" name="Interest[]" value = "Swimming">
-		Day Dreaming <input type="checkbox" name="Interest[]" value = "Day Dreaming">
-	</td></tr>
-	<!-- 7 -->
-	<tr><td>Submit ? <td><input type="Submit" name = "Sub"></td></tr>
-</table>
+</div>
 </form>
